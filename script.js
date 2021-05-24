b9130ed1c32c4ebebabc920786b684e9a40a946e
George Schafer <george.schafer@smartrent.com>
2021-05-23 22:00:38 -0700

    cleaned up imports in script.js

--- script.js
+++ script.js
@@ -1,10 +1,8 @@
 import {
-    createChallenge,
     setScore,
-    getChallengeInteger,
     generateChallenge
 } from './actions.js';
 
-setInterval(()=>generateChallenge(),3000);
+setInterval(() => generateChallenge(), 3000);
 document.body.onload = setScore();
 
